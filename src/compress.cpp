68bb85092015b64480b595c8338e54d3fbffb7e9
Aimee Shao <aimeeshao101@gmail.com>
2019-11-04 02:35:46 +0000

    Completed compress and uncompress for checkpoint

--- src/compress.cpp
+++ src/compress.cpp
@@ -20,8 +20,9 @@
 void pseudoCompression(string inFileName, string outFileName) {
     ifstream in(inFileName, ios::binary);  // open inFile
 
-    HCTree tree;                      // HCTree to build and help encode
-    vector<unsigned int> freqs(256);  // stores frequencies form input file
+    HCTree tree;                            // HCTree to build and help encode
+    const unsigned int ASCII_MAX = 256;     // number of ascii values for HCTree
+    vector<unsigned int> freqs(ASCII_MAX);  // stores freqs from input file
 
     char ch;  // stores character we are reading
 
@@ -40,6 +41,10 @@ void pseudoCompression(string inFileName, string outFileName) {
     while (in.get(ch)) {  // get 1 character and encode until no more to encode
         tree.encode(ch, out);  // output encoding
     }
+
+    // close files
+    in.close();
+    out.close();
 }
 
 /* TODO: True compression with bitwise i/o and small header (final) */
@@ -51,11 +56,11 @@ void trueCompression(string inFileName, string outFileName) {}
  * @param argv Array of arguments
  */
 int main(int argc, char* argv[]) {
-    FileUtils utils;
+    FileUtils utils;  // utilities for checking files
     const char* inFileName = (argc >= 2) ? argv[1] : "";
     const char* outFileName = (argc >= 3) ? argv[2] : "";
 
-    const char* usage =
+    const char* usage =  // usage statement
         "Compresses files using Huffman Encoding\n"
         "Usage:\n"
         "  ./compress [OPTION...] ./path_to_input_file "
--- src/uncompress.cpp
+++ src/uncompress.cpp
@@ -1,7 +1,10 @@
 /**
- * TODO: file header
+ * Uncompresses a given compressed file into a given output filename.
+ * Rebuilds an HCTree using the compressed header to uncompress bits into
+ * symbols.
  *
- * Author:
+ * Author: Aimee T Shao
+ * PID: A15444996
  */
 #include <fstream>
 #include <iostream>
@@ -10,12 +13,72 @@
 #include "HCNode.hpp"
 #include "HCTree.hpp"
 
-/* TODO: Pseudo decompression with ascii encoding and naive header (checkpoint)
+/* Perform pseudo decompression with ascii encoding and naive header
+ * (checkpoint) Read compressed file, build HCTree based header, open
+ * decompressed file, write decoding.
+ * @param inFileName Compressed file to read from
+ * @param outFileName File to write uncompressed file to
  */
-void pseudoDecompression(string inFileName, string outFileName) {}
+void pseudoDecompression(string inFileName, string outFileName) {
+    ifstream in(inFileName, ios::binary);  // open inFile
+
+    HCTree tree;                            // HCTree to build and help decode
+    const unsigned int ASCII_MAX = 256;     // number of ascii values for HCTree
+    vector<unsigned int> freqs(ASCII_MAX);  // stores freqs from input file
+
+    char buf[BUFSIZ];        // Retrieve frequency of ascii values
+    unsigned int index = 0;  // stores ascii value of char's freq we are reading
+
+    while (index < ASCII_MAX) {  // get freq count for each ascii char
+        in.getline(buf, BUFSIZ);
+        unsigned int count = atoi(buf);  // convert buf to unsigned int for freq
+        freqs[index] = count;
+        index++;
+    }
+
+    tree.build(freqs);  // build tree
+
+    ofstream out(outFileName, ios::binary);  // open outFile
+
+    unsigned char decoding;
+    while ((decoding = tree.decode(in))) {
+        out << decoding;  // output encoding
+    }
+
+    // close files
+    in.close();
+    out.close();
+}
 
 /* TODO: True decompression with bitwise i/o and small header (final) */
 void trueDecompression(string inFileName, string outFileName) {}
 
-/* TODO: Main program that runs the uncompress */
-int main(int argc, char* argv[]) { return 0; }
+/* Main program that runs the uncompress. Checks if input file is invalid or
+ * empty.
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ */
+int main(int argc, char* argv[]) {
+    FileUtils utils;  // utilities for checking files
+    const char* inFileName = (argc >= 2) ? argv[1] : "";
+    const char* outFileName = (argc >= 3) ? argv[2] : "";
+
+    const char* usage =  // usage statement
+        "Compresses files using Huffman Encoding\n"
+        "Usage:\n"
+        "  ./uncompress [OPTION...] ./path_to_compressed_input_file "
+        "./path_to_output_file\n\n"
+        "      --ascii\tWrite output in ascii mode instead of bit "
+        "stream\n"
+        "  -h, --help\tPrint help and exit\n";
+
+    if (!utils.isValidFile(inFileName)) {  // print error and usage
+        cout << usage << endl;
+        return 0;
+    }
+
+    // No error, then compress
+    pseudoDecompression(inFileName, outFileName);
+
+    return 0;
+}
