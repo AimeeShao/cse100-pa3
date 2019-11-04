6d9ff7542169f42d3f0c580aff06377dca6df095
Aimee Shao <aimeeshao101@gmail.com>
2019-11-04 01:45:02 +0000

    Completed compress for checkpoint

--- src/compress.cpp
+++ src/compress.cpp
@@ -1,7 +1,9 @@
 /**
- * TODO: file header
+ * Compresses a given file into a given output filename. Uses the Huffman
+ * HCTree to compress characters into bits.
  *
- * Author:
+ * Author: Aimee T Shao
+ * PID: A15444996
  */
 #include <fstream>
 #include <iostream>
@@ -10,12 +12,65 @@
 #include "HCNode.hpp"
 #include "HCTree.hpp"
 
-/* TODO: add pseudo compression with ascii encoding and naive header
- * (checkpoint) */
-void pseudoCompression(string inFileName, string outFileName) {}
+/* Perform pseudo compression with ascii encoding and naive header
+ * (checkpoint). Read first file, build HCTree based on frequencies of each char
+ * in first file, open compressed file, write header, then encoding.
+ * @param inFileName File to read from
+ * @param outFileName File to write compressed file to */
+void pseudoCompression(string inFileName, string outFileName) {
+    ifstream in(inFileName, ios::binary);  // open inFile
+
+    HCTree tree;                      // HCTree to build and help encode
+    vector<unsigned int> freqs(256);  // stores frequencies form input file
+
+    char ch;  // stores character we are reading
+
+    while (in.get(ch)) {  // get 1 character each time until no more
+        freqs[ch]++;
+    }
+
+    tree.build(freqs);  // build tree
+
+    ofstream out(outFileName, ios::binary);  // open outFile
+    for (unsigned int freq : freqs) {        // output header
+        out << freq << endl;
+    }
+
+    in = ifstream(inFileName, ios::binary);  // reopen inFile
+    while (in.get(ch)) {  // get 1 character and encode until no more to encode
+        tree.encode(ch, out);  // output encoding
+    }
+}
 
 /* TODO: True compression with bitwise i/o and small header (final) */
 void trueCompression(string inFileName, string outFileName) {}
 
-/* TODO: Main program that runs the compress */
-int main(int argc, char* argv[]) { return 0; }
+/* Main program that runs the compress. Checks if input file is invalid or
+ * empty.
+ * @param argc Number of arguments
+ * @param argv Array of arguments
+ */
+int main(int argc, char* argv[]) {
+    FileUtils utils;
+    const char* inFileName = (argc >= 2) ? argv[1] : "";
+    const char* outFileName = (argc >= 3) ? argv[2] : "";
+
+    const char* usage =
+        "Compresses files using Huffman Encoding\n"
+        "Usage:\n"
+        "  ./compress [OPTION...] ./path_to_input_file "
+        "./path_to_output_file\n\n"
+        "      --ascii\tWrite output in ascii mode instead of bit "
+        "stream\n"
+        "  -h, --help\tPrint help and exit\n";
+
+    if (!utils.isValidFile(inFileName)) {  // print error and usage
+        cout << usage << endl;
+        return 0;
+    }
+
+    // No error, then compress
+    pseudoCompression(inFileName, outFileName);
+
+    return 0;
+}
